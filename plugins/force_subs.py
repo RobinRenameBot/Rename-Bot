06cfe0f26fa87bd0bb3df7b6db87953c2e014246
CaptainUsoppBot <inanstcompany@gmail.com>
2024-07-17 18:46:56 +0600

    Update force_subs.py

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -21,12 +21,12 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="🔺 Update Channel 🔺", url=f"https://t.me/{Config.FORCE_SUB}") ]]
-    text = "<b>Hello Dear \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>"
+    buttons = [[InlineKeyboardButton(text="○ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴀʟ ○", url=f"https://t.me/{Config.FORCE_SUB}") ]]
+    text = "<b>Hᴇʟʟᴏ Usᴇʀs \n\nIғ Yᴏᴜ Wᴀɴᴛ Tᴏ Usᴇ Mᴇ Tʜᴇɴ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴀʟ Nᴏᴡ</b>"
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
         if user.status == enums.ChatMemberStatus.BANNED:                                   
-            return await client.send_message(message.from_user.id, text="Sorry You Are Banned To Use Me")  
+            return await client.send_message(message.from_user.id, text="Sᴏʀʀʏ Yᴏᴜ Aʀᴇ Bᴀɴɴᴇᴅ Fʀᴏᴍ Usᴇɪɴɢ Mᴇ Cᴏɴᴛᴀᴄᴛ Oᴡɴᴇʀ")  
     except UserNotParticipant:                       
         return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
     return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
