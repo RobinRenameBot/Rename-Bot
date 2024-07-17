aa899d8d2355943fe29e2dfa8859b76e3f99ec33
CaptainUsoppBot <inanstcompany@gmail.com>
2024-07-17 18:19:41 +0600

    Update config.py

--- config.py
+++ config.py
@@ -74,12 +74,7 @@ Fᴏʀ Mᴏʀᴇ Hᴇʟᴘ Sᴇɴᴅ Mᴇ A Qᴜɪᴄᴋ Mᴇssᴀɢᴇ :- <a hr
 #ʀᴀᴘᴏ ᴄʀᴇᴀᴛᴏʀ https://github.com/AshutoshGoswami24
 #ʀᴀᴘᴏ https://github.com/AshutoshGoswami24/Rename-Bot
     DONATE_TXT = """
-<b>𝗧𝗵𝗮𝗻𝗸𝘀 𝗙𝗼𝗿 𝗦𝗵𝗼𝘄𝗶𝗻𝗴 𝗜𝗻𝘁𝗲𝗿𝗲𝘀𝘁 𝗜𝗻 𝗗𝗼𝗻𝗮𝘁𝗶𝗼𝗻! ❤️</b>
-
-𝐈𝐟 𝐘𝐨𝐮 𝐋𝐢𝐤𝐞 𝐌𝐲 𝐁𝐨𝐭𝐬 & 𝐏𝐫𝐨𝐣𝐞𝐜𝐭𝐬, 𝐘𝐨𝐮 𝐂𝐚𝐧 🎁 𝐃𝐨𝐧𝐚𝐭𝐞 𝐌𝐞 𝐀𝐧𝐲 𝐀𝐦𝐨𝐮𝐧𝐭 𝐅𝐫𝐨𝐦 𝟏𝟎𝐌 𝐑𝐬 😁 𝐔𝐩𝐭𝐨 𝐘𝐨𝐮𝐫 𝐂𝐡𝐨𝐢𝐜𝐞.
-
-<b>🛍 𝗨𝗣𝗜 𝗜𝗗:</b> `PandaWep@ybl`
-"""
+<b>Dᴏɴᴀᴛᴇ Tʜᴏᴏsᴇ Wʜᴏ Rᴇᴀʟʟʏ Nᴇᴇᴅ Nᴏᴛ Mᴇ! ❤️</b>
 
 #ʀᴀᴘᴏ ᴄʀᴇᴀᴛᴏʀ https://github.com/AshutoshGoswami24
 #ʀᴀᴘᴏ https://github.com/AshutoshGoswami24/Rename-Bot
